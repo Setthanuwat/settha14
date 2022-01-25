6b1b352fb570023ecad1330daa0592fed2c75130
Setthanuwat <aftnick2@gmail.com>
2022-01-25 20:32:36 +0700

    "Update"

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -4,6 +4,7 @@
 #include<string>
 #include<cstdlib>
 
+
 using namespace std;
 
 char score2grade(int score){
@@ -20,20 +21,116 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scores,vector<char> &grades)
+{
+    ifstream data(filename);
+    string s ;
+    char format[] ="%[^:]: %d %d %d";
+    int a;
+    int b;
+    int c;
+    int score;
+    char name[23];
+    while (getline(data,s))
+    {
+        const char* cs = s.c_str();
+       sscanf(cs,format,name,&a,&b,&c);
+        score = a+b+c;
+        scores.push_back(score);
+        names.push_back(name);
+        grades.push_back(score2grade(score));
+    }
+}
+
+void getCommand(string &command,string &key)
+{
+    string s;
+    char a[20], b[20];
+    cout << "Please input your command: ";
+    getline(cin,s);
+    const char *text = s.c_str();
+    char format[] = "%s %[^:] ";
+    sscanf(text,format,&a,&b);
+    command = a;
+    key = b;
+    
 
 }
 
-void getCommand(){
-
+void searchName(vector<string>names, vector<int>scores, vector<char>grades, string key)
+{
+    ifstream data("name_score.txt");
+    string s;
+    string M;
+    char format[] ="%[^:]: %d %d %d";
+    int a,b,c;
+    int i = 0;
+    int x = 0;
+    char name[23];
+    cout << "---------------------------------\n";
+    while (getline(data,s))
+    {
+        
+        const char* cs = s.c_str();
+       sscanf(cs,format,name,&a,&b,&c);
+        M = toUpperStr(name);
+        if(key == M)
+        {
+            cout << names[i] << "'s score = " <<scores[i] << "\n";
+             cout << names[i] <<"'s grade = "<< grades[i] << "\n";
+             x++;
+        }
+        i++;
+    }
+    if(x == 0)
+    {  
+            cout << "Cannot found." << endl;
+    }
+        cout << "---------------------------------\n";
 }
 
-void searchName(){
-
-}
-
-void searchGrade(){
-
+void searchGrade(vector<string>names, vector<int>scores, vector<char>grades, string key)
+{
+    ifstream data("name_score.txt");
+    string s;
+    string M;
+    char format[] ="%[^:]: %d %d %d";
+    int a,b,c;
+    int i = 0;
+    int score;
+    char grade;
+    char name[23];
+    cout << "---------------------------------\n";
+    while (getline(data,s))
+    {
+        
+        const char* cs = s.c_str();
+       sscanf(cs,format,name,&a,&b,&c);
+        score = a+b+c;
+        grade = score2grade(score);
+        if(key == "A" && grade == 'A')
+            {
+                cout << names[i] << " " <<"("<<scores[i] << ")" <<  "\n";
+            }
+        if(key == "B" && grade == 'B')
+            {
+                cout << names[i] << " " <<"("<<scores[i] << ")" <<  "\n";
+            }
+        if(key == "C" && grade == 'C')
+            {
+                cout << names[i] << " " <<"("<<scores[i] << ")" <<  "\n";
+            }
+        if(key == "D" && grade == 'D')
+            {
+                cout << names[i] << " " <<"("<<scores[i] << ")" <<  "\n";
+            }
+        if(key == "F" && grade == 'F')
+            {
+                cout << names[i] << " " <<"("<<scores[i] << ")" <<  "\n";
+            } 
+            i++;         
+    }
+    cout << "---------------------------------\n";
 }
 
 
@@ -43,7 +140,7 @@ int main(){
     vector<int> scores;
     vector<char> grades; 
     importDataFromFile(filename, names, scores, grades);
-    
+
     do{
         string command, key;
         getCommand(command,key);
